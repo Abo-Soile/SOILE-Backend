3d8c68d1e1f84b7b2b8a209de31fbfec3a55ae62
Daniel Wärnå <daniel.warna@gmail.com>
2014-03-24 16:01:04 +0200

    Started implementing the required functionality in textareas, will need to extend the dojo widget a little to make this work.
    Might as well create a custom build and buildscript for dojo and add a folder with extensions

---
+++ compileDojo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir dojoCompiler
+cd dojoCompiler
+
+curl -sS http://download.dojotoolkit.org/release-1.9.3/dojo-release-1.9.3-src.tar.gz > dojo.tar.gz
+tar xvf dojo.tar.gz
--- prod/mods/fi.abo.kogni.soile~http-server~1.0/static_files/javascript/formphase.js
+++ prod/mods/fi.abo.kogni.soile~http-server~1.0/static_files/javascript/formphase.js
@@ -57,6 +57,18 @@ require(["dijit/form/HorizontalSlider",
       _mouseWheeled: function() {}
     });
 
+    define(["dojo/_base/declare", "dojo/_base/lang", "dijit/form/SimpleTextarea", "dijit/form/ValidationTextBox"],
+      function(declare, lang, SimpleTextarea, ValidationTextBox) {
+
+        return declare('dijit.form.ValidationTextArea', [SimpleTextarea, ValidationTextBox], {
+          constructor: function(params){
+            this.constraints = {};
+            this.baseClass += ' dijitValidationTextArea';
+          },    
+          templateString: "<textarea ${!nameAttrSetting} data-dojo-attach-point='focusNode,containerNode,textbox' autocomplete='off'></textarea>"
+        })
+      })
+
     var qdata = {};
     var testdata = {};
 
