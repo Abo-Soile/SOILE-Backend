6e46c1d1d760353ec04e66f3916f45f60e18d3f0
Daniel Wärnå <daniel@danielwarna.com>
2014-11-19 10:24:33 +0200

    Woff files are sent by vertx

--- prod/mods/fi.abo.kogni.soile~http-server~1.0/http_server.js
+++ prod/mods/fi.abo.kogni.soile~http-server~1.0/http_server.js
@@ -1585,7 +1585,7 @@ customMatcher.get('/', function(request) {
 
 /* This will match static files. ('Static files' are files which 
  * have not been generated programmatically.) */
-customMatcher.allWithRegEx('.*\.(html|htm|css|js|png|jpg|jpeg|gif|ico|md|wof|ttf|svg)$', function(req) {
+customMatcher.allWithRegEx('.*\.(html|htm|css|js|png|jpg|jpeg|gif|ico|md|wof|ttf|svg|woff)$', function(req) {
   req.response.sendFile(utils.file_from_serverdir(req.path()));
 });
 
