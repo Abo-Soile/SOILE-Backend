26ce98524ad8193f3f38e27c82405e421aa539ec
Daniel Wärnå <daniel@danielwarna.com>
2015-04-20 09:59:53 +0300

    Empty labels are not rendered

--- http_server/src/test/frontend tests/testAggregation.js
+++ http_server/src/test/frontend tests/testAggregation.js
@@ -49,5 +49,78 @@ describe("Aggragate data functions with stored data", function() {
     expect(a.length).toBe(3);
     expect(b.length).toBe(9);
   })
+
+  it("testRandommapping", function() {
+    var randomList =    [0,1,2,3,4,5,6,7,8,9];
+    var randomMapping = [0,1,2,3,4,5,6,7,8,9];
+    var randomT =       [1,1,0,2,3,3,2,2,1,1];
+    var randomGroups =  [0,0,0,0,0,0,0,0,0,0]
+
+
+    for (var i = 0; i < randomT.length; i++) {
+      if (randomT[i]>0) {
+        randomGroups[randomT[i]] = 1;
+      }
+      randomMapping[i] = i;
+    };
+
+    console.log(randomMapping);
+    console.log(randomT);
+    console.log(randomGroups);
+
+    var shuffle =function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex ;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    }
+
+    function randomizeGroup(array, groupMapping, groupNo) {
+      var tempArr = [];
+      for (var i = 0; i < array.length; i++) {
+        if(groupMapping[i]===groupNo) {
+          tempArr.push(array[i])
+          array[i] = null;
+        }
+      };
+      console.log(array)
+      tempArr = shuffle(tempArr);
+
+      for (var i = 0; i < array.length; i++) {
+        if(array[i] === null) {
+          array[i] = tempArr.pop();
+        }
+      };
+
+      return array;
+
+    }
+
+    console.log(randomGroups);
+    console.log("-------------------")
+
+    for (var i = 0; i < randomGroups.length; i++) {
+      if(randomGroups[i]===1) {
+        randomMapping = randomizeGroup(randomMapping, randomT, i)
+      }
+    };
+
+
+    console.log(randomMapping)
+
+
+  })
+
 });
 
