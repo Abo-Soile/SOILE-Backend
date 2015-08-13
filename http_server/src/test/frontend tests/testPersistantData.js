4c1afbbf8c11ca810689f58459968dce20daae81
Daniel Wärnå <daniel@danielwarna.com>
2015-08-13 14:20:34 +0300

    Add, testing loading of persistant variables from external source.

--- http_server/src/test/frontend tests/testPersistantData.js
+++ http_server/src/test/frontend tests/testPersistantData.js
@@ -19,4 +19,19 @@ describe("persistant data", function() {
 
     expect(variable).toBe(10);
   });
+
+  it("load values from file", function() {
+    var variables = {"var1":1, "var2":2};
+    SOILE2.rt.persistantDataHandler.set(variables);
+
+    var var1 = SOILE2.bin.loadvariable("var1", 99);
+    var var2 = SOILE2.bin.loadvariable("var2", 99);
+    var var3 = SOILE2.bin.loadvariable("var3", 99);
+
+    expect(var1).toBe(1);
+    expect(var2).toBe(2);
+    expect(var3).toBe(99);
+  });
+  
+
 });
