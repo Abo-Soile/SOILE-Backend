c61eed4e7bb5727ec5fa5c4f2d7e8da63773e548
Daniel Wärnå <daniel@danielwarna.com>
2015-09-16 15:15:12 +0300

    Fix project setup and imports
    Module dependencies are placed in the module lib folder and
    not in the common resources as before, avoiding namespace clashes
    when some module uses another version of a library.
    
    All project settings are stored in IDEAs format as .iml files, and
    the old .classpath files for eclipse are ignored.
    
    Recompiled the elang parser and lexer with the latest version of antlr.
    The elang compiler is using the latest version as well.
    
    The older version of antlr 4.0 is still used for the questioneer builder.
    As there seem to be some incompativilities...
    
    Make sure that old libs are removed before deplying this!!

--- deploy_jars.sh
+++ deploy_jars.sh
@@ -15,7 +15,7 @@ $ANT -f soile-elang/build.xml makejar
 $ANT -f soile-utils/build.xml makejar
 $ANT -f soile-verticle/build.xml makejar
 
-cp -avf soile-qmarkup/jar/soile2-qmarkup.jar $TO
-cp -avf soile-elang/jar/soile2-elang.jar  $TO
+cp -avf soile-qmarkup/jar/soile2-qmarkup.jar prod/mods/fi.abo.kogni.soile~questionnaire-render~1.0/lib
+cp -avf soile-elang/jar/soile2-elang.jar  prod/mods/fi.abo.kogni.soile~experiment-lang~1.0/lib
 cp -avf soile-utils/jar/soile2-utils.jar $TO
 cp -avf soile-verticle/jar/soile2-verticle.jar $TO
