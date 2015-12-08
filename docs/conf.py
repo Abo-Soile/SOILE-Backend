6c029faff8ab1fdde6079f5df6e044a318aaa21f
Daniel Wärnå <daniel.warna@gmail.com>
2015-12-08 11:40:59 +0000

    Fix extra whitespace in sphinx config path

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
@@ -169,7 +169,7 @@ html_theme_options = {
     'globaltoc_includehidden': "true",
 }
 
-html_sidebars = {'functions': ['localtoc.html',], "index": ["customSidebar.html "], '*': ['localtoc.html']}
+html_sidebars = {'functions': ['localtoc.html',], "index": ["customSidebar.html"], '*': ['localtoc.html']}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
