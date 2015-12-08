0f2185e1d3dd470d33a2c93cf15662b9dbd6a225
Daniel Wärnå <daniel@danielwarna.com>
2015-12-08 15:23:35 +0200

    Update the append test to use the new functionality

--- http_server/src/test/frontend tests/testAppend.js
+++ http_server/src/test/frontend tests/testAppend.js
@@ -5,6 +5,7 @@ describe("Builtin append", function() {
     app = SOILE2.bin.append;
   });
 
+
   it("appends 2 strings", function() {
 
     console.log(app("rt-",0));
@@ -15,6 +16,20 @@ describe("Builtin append", function() {
     expect(twoStrings1).toBe("s1s2");
     expect(twoStrings2).toBe("s2s1");
   });
+
+  it("appends multiple strings", function() {
+    console.log("MULTIPLE STRINGS\n\n");
+
+    var fiveStrings = app("s1", "s2", "s3", "s4", "s5");
+    var arrayFirst = app([1], 2,5,6,8,3,55);
+
+    var mixedAppend = app(1, "asda", [1,2,3], {a:"a",b:"b"});
+
+    expect(fiveStrings).toBe("s1s2s3s4s5");
+    expect(arrayFirst).toEqual([1,2,5,6,8,3,55]);
+    expect(mixedAppend).toEqual("1asda1,2,3[object Object]");
+  });
+
 
   it("appends arrays", function() {
     var arrEmpty = [];
