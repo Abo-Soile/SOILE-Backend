ec6756d682177603ca94925010631b653d72e22f
Daniel Wärnå <daniel.warna@gmail.com>
2020-01-07 02:00:27 +0200

    Improve mods clone script

--- prod/clone_vertx_mods.sh
+++ prod/clone_vertx_mods.sh
@@ -8,15 +8,16 @@ version="1.0"
 echo "Fetching dependencies"
 
 #Dust.js templates
-if [ ! -d "fi.abo.kogni.soile~vertx-mod-template-engines~1.0
-" ]; then
+if [ ! -d "fi.abo.kogni.soile~vertx-mod-template-engines~1.0" ]; then
 	#git clone git@github.com:danielwarna/vertx-mod-template-engines.git
-  echo "Cloning dust"
+	echo "Cloning dust"
 	git clone https://github.com/danielwarna/vertx-mod-template-engines.git
 	echo "Renaming folder to $domain~vertx-mod-template-engines~$version"
 
-  rm $domain~vertx-mod-template-engines~$version -rf
-	mv -vf vertx-mod-template-engines $domain~vertx-mod-template-engines~$version
+	rm $domain~vertx-mod-template-engines~$version -rf
+	# mv -vf vertx-mod-template-engines $domain~vertx-mod-template-engines~$version
+	cp -r vertx-mod-template-engines $domain~vertx-mod-template-engines~$version
+	rm vertx-mod-template-engines
 fi
 
 #Vertex mongo persistor
